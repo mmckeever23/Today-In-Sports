7026781732d4a8f919cc1cce3e549d9ffe34a2a7
Matt McKeever <mmckeever23@gmail.com>
2022-08-22 17:55:35 -0500

    Changed some highlighting

--- script.js
+++ script.js
@@ -29,22 +29,22 @@ window.addEventListener("load", function() {
                             gamesList1.innerHTML += `   
                             <p><b>${data.games[i].teams.home.team.name}</b> <i>(${json.dates[0].games[i].teams.home.leagueRecord.wins}-${json.dates[0].games[i].teams.home.leagueRecord.losses})</i> vs. <b>${json.dates[0].games[i].teams.away.team.name}</b> <i>(${json.dates[0].games[i].teams.away.leagueRecord.wins}-${json.dates[0].games[i].teams.away.leagueRecord.losses})</i><br>
                             Game ${data.games[i].seriesGameNumber} of a ${json.dates[0].games[i].gamesInSeries}-game series @ ${json.dates[0].games[i].venue.name}<br>
-                            <b>UPCOMING<br>
-                            Game begins to${data.games[i].dayNight} at ${dateToTime(localDate)}</b></p>
+                            <b>UPCOMING</b><br>
+                            Game begins to${data.games[i].dayNight} at ${dateToTime(localDate)}</p>
                             `;
                         } else if (data.games[i].status.detailedState == "Warmup") {
                             gamesList1.innerHTML += `   
                             <p class="p-red"><b>${data.games[i].teams.home.team.name}</b> <i>(${json.dates[0].games[i].teams.home.leagueRecord.wins}-${json.dates[0].games[i].teams.home.leagueRecord.losses})</i> vs. <b>${json.dates[0].games[i].teams.away.team.name}</b> <i>(${json.dates[0].games[i].teams.away.leagueRecord.wins}-${json.dates[0].games[i].teams.away.leagueRecord.losses})</i><br>
                             Game ${data.games[i].seriesGameNumber} of a ${json.dates[0].games[i].gamesInSeries}-game series @ ${json.dates[0].games[i].venue.name}<br>
-                            <b>STARTING SOON<br>
-                            Game begins to${data.games[i].dayNight} at ${dateToTime(localDate)}</b></p>
+                            <b>STARTING SOON</b><br>
+                            Game begins to${data.games[i].dayNight} at ${dateToTime(localDate)}</p>
                             `;
                         } else if (data.games[i].status.detailedState == "In Progress") {
                             gamesList1.innerHTML += ` 
                             <p class="p-green"><b>${data.games[i].teams.home.team.name}</b> <i>(${json.dates[0].games[i].teams.home.leagueRecord.wins}-${json.dates[0].games[i].teams.home.leagueRecord.losses})</i> vs. <b>${json.dates[0].games[i].teams.away.team.name}</b> <i>(${json.dates[0].games[i].teams.away.leagueRecord.wins}-${json.dates[0].games[i].teams.away.leagueRecord.losses})</i><br>
                             Game ${data.games[i].seriesGameNumber} of a ${json.dates[0].games[i].gamesInSeries}-game series @ ${json.dates[0].games[i].venue.name}<br>  
-                            <b>IN PROGRESS<br>
-                            ${json.dates[0].games[i].teams.home.team.name} - ${json.dates[0].games[i].teams.home.score}, ${json.dates[0].games[i].teams.away.team.name} - ${json.dates[0].games[i].teams.away.score}</b></p>
+                            <b>IN PROGRESS</b<<br>
+                            ${json.dates[0].games[i].teams.home.team.name} - ${json.dates[0].games[i].teams.home.score}, ${json.dates[0].games[i].teams.away.team.name} - ${json.dates[0].games[i].teams.away.score}</p>
                             `;
                         } else if ((data.games[i].status.detailedState == "Game Over" || data.games[i].status.detailedState == "Final") && data.games[i].teams.away.isWinner == true) {
                             gamesList1.innerHTML += `   
@@ -68,22 +68,22 @@ window.addEventListener("load", function() {
                             gamesList2.innerHTML += `   
                             <p><b>${data.games[i].teams.home.team.name}</b> <i>(${json.dates[0].games[i].teams.home.leagueRecord.wins}-${json.dates[0].games[i].teams.home.leagueRecord.losses})</i> vs. <b>${json.dates[0].games[i].teams.away.team.name}</b> <i>(${json.dates[0].games[i].teams.away.leagueRecord.wins}-${json.dates[0].games[i].teams.away.leagueRecord.losses})</i><br>
                             Game ${data.games[i].seriesGameNumber} of a ${json.dates[0].games[i].gamesInSeries}-game series @ ${json.dates[0].games[i].venue.name}<br>
-                            <b>UPCOMING<br>
-                            Game begins to${data.games[i].dayNight} at ${dateToTime(localDate)}</b></p>
+                            <b>UPCOMING</b><br>
+                            Game begins to${data.games[i].dayNight} at ${dateToTime(localDate)}</p>
                             `;
                         } else if (data.games[i].status.detailedState == "Warmup") {
                             gamesList2.innerHTML += `   
                             <p class="p-red"><b>${data.games[i].teams.home.team.name}</b> <i>(${json.dates[0].games[i].teams.home.leagueRecord.wins}-${json.dates[0].games[i].teams.home.leagueRecord.losses})</i> vs. <b>${json.dates[0].games[i].teams.away.team.name}</b> <i>(${json.dates[0].games[i].teams.away.leagueRecord.wins}-${json.dates[0].games[i].teams.away.leagueRecord.losses})</i><br>
                             Game ${data.games[i].seriesGameNumber} of a ${json.dates[0].games[i].gamesInSeries}-game series @ ${json.dates[0].games[i].venue.name}<br>
-                            <b>STARTING SOON<br>
-                            Game begins to${data.games[i].dayNight} at ${dateToTime(localDate)}</b></p>
+                            <b>STARTING SOON</b><br>
+                            Game begins to${data.games[i].dayNight} at ${dateToTime(localDate)}</p>
                             `;
                         } else if (data.games[i].status.detailedState == "In Progress") {
                             gamesList2.innerHTML += ` 
                             <p class="p-green"><b>${data.games[i].teams.home.team.name}</b> <i>(${json.dates[0].games[i].teams.home.leagueRecord.wins}-${json.dates[0].games[i].teams.home.leagueRecord.losses})</i> vs. <b>${json.dates[0].games[i].teams.away.team.name}</b> <i>(${json.dates[0].games[i].teams.away.leagueRecord.wins}-${json.dates[0].games[i].teams.away.leagueRecord.losses})</i><br>
                             Game ${data.games[i].seriesGameNumber} of a ${json.dates[0].games[i].gamesInSeries}-game series @ ${json.dates[0].games[i].venue.name}<br>  
-                            <b>IN PROGRESS<br>
-                            ${json.dates[0].games[i].teams.home.team.name} - ${json.dates[0].games[i].teams.home.score}, ${json.dates[0].games[i].teams.away.team.name} - ${json.dates[0].games[i].teams.away.score}</b></p>
+                            <b>IN PROGRESS</b><br>
+                            ${json.dates[0].games[i].teams.home.team.name} - ${json.dates[0].games[i].teams.home.score}, ${json.dates[0].games[i].teams.away.team.name} - ${json.dates[0].games[i].teams.away.score}</p>
                             `;
                         } else if ((data.games[i].status.detailedState == "Game Over" || data.games[i].status.detailedState == "Final") && data.games[i].teams.away.isWinner == true) {
                             gamesList2.innerHTML += `   
@@ -107,15 +107,15 @@ window.addEventListener("load", function() {
                             gamesList3.innerHTML += `   
                             <p><b>${data.games[i].teams.home.team.name}</b> <i>(${json.dates[0].games[i].teams.home.leagueRecord.wins}-${json.dates[0].games[i].teams.home.leagueRecord.losses})</i> vs. <b>${json.dates[0].games[i].teams.away.team.name}</b> <i>(${json.dates[0].games[i].teams.away.leagueRecord.wins}-${json.dates[0].games[i].teams.away.leagueRecord.losses})</i><br>
                             Game ${data.games[i].seriesGameNumber} of a ${json.dates[0].games[i].gamesInSeries}-game series @ ${json.dates[0].games[i].venue.name}<br>
-                            <b>UPCOMING<br>
-                            Game begins to${data.games[i].dayNight} at ${dateToTime(localDate)}</b></p>
+                            <b>UPCOMING</b><br>
+                            Game begins to${data.games[i].dayNight} at ${dateToTime(localDate)}</p>
                             `;
                         } else if (data.games[i].status.detailedState == "Warmup") {
                             gamesList3.innerHTML += `   
                             <p class="p-red"><b>${data.games[i].teams.home.team.name}</b> <i>(${json.dates[0].games[i].teams.home.leagueRecord.wins}-${json.dates[0].games[i].teams.home.leagueRecord.losses})</i> vs. <b>${json.dates[0].games[i].teams.away.team.name}</b> <i>(${json.dates[0].games[i].teams.away.leagueRecord.wins}-${json.dates[0].games[i].teams.away.leagueRecord.losses})</i><br>
                             Game ${data.games[i].seriesGameNumber} of a ${json.dates[0].games[i].gamesInSeries}-game series @ ${json.dates[0].games[i].venue.name}<br>
-                            <b>STARTING SOON<br>
-                            Game begins to${data.games[i].dayNight} at ${dateToTime(localDate)}</b></p>
+                            <b>STARTING SOON</b><br>
+                            Game begins to${data.games[i].dayNight} at ${dateToTime(localDate)}</p>
                             `;
                         } else if (data.games[i].status.detailedState == "In Progress") {
                             gamesList3.innerHTML += ` 
